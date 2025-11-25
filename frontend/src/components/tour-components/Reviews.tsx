33726888dede26dcee366b2331c409260aeab40f
Ana Arevadze <anaarevadze@Anas-MacBook-Air.local>
2025-11-25 15:57:46 +0400

    Update types/index.ts with Reviews related types

--- frontend/src/components/tour-components/Reviews.tsx
+++ frontend/src/components/tour-components/Reviews.tsx
@@ -1,14 +1,25 @@
 import { useFetchReviews } from "../../hooks/useFetchReviews";
 import ReviewCard from "./ReviewCard";
+import type { Review } from "../../types/index";
 
 const Reviews = () => {
-  const { reviews } = useFetchReviews();
+  const { reviews } = useFetchReviews() as { reviews: Review[] };
   console.log(reviews);
+
+  if (!reviews || reviews.length === 0) return null;
 
   return (
     <section className="section-reviews">
       <div className="reviews">
-        <ReviewCard reviews={reviews} />
+        {reviews.map((review) => (
+          <ReviewCard
+            key={review.id}
+            userPhoto={`/img/users/${review.user.photo}`}
+            userName={review.user.name}
+            rating={review.rating}
+            userReview={review.review}
+          />
+        ))}
       </div>
     </section>
   );
