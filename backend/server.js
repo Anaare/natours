d1c2e093ffa5e0ea1c3284a97ab628f91c7ce79b
Ana Arevadze <anaarevadze@Anas-MacBook-Air.local>
2025-12-04 11:18:59 +0400

    minor bug fix related to types causing errors while trying to deploying the app

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,40 @@
 const mongoose = require('mongoose');
-
 const dotenv = require('dotenv');
 
 process.on('uncaughtException', (err) => {
   console.log('Uncaught exception 💥 shutting down...');
   console.log(err.name, err.message);
-  process.exit(1); // 1 stands for uncaught exception
+
+  process.exit(1);
 });
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
+let server;
 mongoose
   .connect(process.env.DATABASE)
-  .then(() => console.log('DB connection successfull'));
+  .then(() => {
+    console.log('DB connection successful');
 
-const port = process.env.PORT || 8000;
+    const port = process.env.PORT || 8000;
 
-const server = app.listen(port, () => {
-  console.log(`app running on port ${port}...`);
-});
+    server = app.listen(port, () => {
+      console.log(`App running on port ${port}...`);
+    });
 
-process.on('unhandledRejection', (err) => {
-  console.log('Unhandled rejection 💥 shutting down...');
-  console.log(err);
-  server.close(() => {
-    process.exit(1); // 1 stands for uncaught exception
+    process.on('unhandledRejection', (err) => {
+      console.log('Unhandled rejection 💥 shutting down...');
+      console.log(err);
+
+      server.close(() => {
+        process.exit(1);
+      });
+    });
+  })
+  .catch((err) => {
+    console.log('FATAL: DB connection failed 💣');
+    console.error(err.message);
+    process.exit(1);
   });
-});
