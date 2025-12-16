68a447e2e8d8ba64ad265511581eeeaea72395c2
Ana Arevadze <anaarevadze@Anas-MacBook-Air.local>
2025-12-16 15:19:43 +0400

    feat: Integrate Stripe for tour checkout and booking
    Implements the server-side logic to create a Stripe Checkout Session for tour bookings. Defines the POST /api/v1/bookings/checkout-session/:tourId route and the corresponding controller to handle payment initiation.

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,7 @@ const appError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
@@ -114,6 +115,7 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookings', bookingRouter);
 
 // Will run for ALL http verbs
 // Handling UNHANDLED routes
--- backend/controllers/reviewController.js
+++ backend/controllers/reviewController.js
@@ -12,32 +12,3 @@ exports.getAllReviews = factory.getAll(Review);
 exports.createReview = factory.createOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
 exports.updateReview = factory.updateOne(Review);
-
-// exports.createReview = catchAsync(async (req, res) => {
-//   // Allowing nested routes (if it is NOT specified)
-//   if (!req.body.tour) req.body.tour = req.params.tourId;
-//   if (!req.body.user) req.body.user = req.user.id;
-
-//   const newReview = await Review.create(req.body);
-
-//   res.status(201).json({
-//     status: 'success',
-//     data: {
-//       review: newReview,
-//     },
-//   });
-// });
-
-// exports.getAllReviews = catchAsync(async (req, res) => {
-//   let filter = {};
-//   if (req.params.tourId) filter = { tour: req.params.tourId };
-//   const reviews = await Review.find(filter);
-
-//   res.status(200).json({
-//     status: 'success',
-//     results: reviews.length,
-//     data: {
-//       reviews,
-//     },
-//   });
-// });
