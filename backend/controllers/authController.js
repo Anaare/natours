dbc105cd3bca0c18f0efa7896844a653073c885a
Ana Arevadze <anaarevadze@Anas-MacBook-Air.local>
2025-12-18 13:01:04 +0400

    Update JWT expiresIn

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -16,10 +16,9 @@ const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
   const isProduction = process.env.NODE_ENV === 'production';
+  const cookieExpiresIn = 90;
   const cookieOptions = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
-    ),
+    expires: new Date(Date.now() + cookieExpiresIn * 24 * 60 * 60 * 1000),
     httpOnly: true,
     secure: isProduction,
     sameSite: isProduction ? 'none' : 'lax',
